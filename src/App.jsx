17ffd6545aeca5d4367a07933f69db14c54a9f23
User <User@LAB3-3>
2024-03-19 09:32:49 +0200

    finished diseaseinfo

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import './App.css'
 function App() {
     return (
         <>
-            <div className='top'>
+            <div className='top' style={{
+                position: 'fixed'
+            }}>
                 <h1>modelcomp results browser</h1>
             </div>
         </>
