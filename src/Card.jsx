85839da7eb5fca3e46cddd820d2a93eb1438104f
Gil Ramot <gilramot@github.com>
2024-03-31 17:03:49 +0300

    added numbers of research stats

--- src/Card.jsx
+++ src/Card.jsx
@@ -6,8 +6,21 @@ function Card() {
     const [isShown, setIsShown] = useState(false)
     return (
         <>
+            <div style={{
+                position: "fixed",
+                left: 20,
+                top: 400,
+                visibility: isShown ? 'hidden' : 'visible'
+            }}>
+                <span className="big-number">313</span>
+                <span className="small-text">types of bacteria (1938 total)</span>
+                <br/>
+                <span className="big-number">1818</span>
+                <span className="small-text">humans tested</span>
+            </div>
             <div id='card' className='card' style={{visibility: isShown ? 'hidden' : 'visible'}}>
-                <QRCode bgColor='#1a1a1a' fgColor='#a1a1a1' value='https://pypi.org/project/modelcomp/' className='card-image'/>
+            <QRCode bgColor='#1a1a1a' fgColor='#a1a1a1' value='https://pypi.org/project/modelcomp/'
+                        className='card-image'/>
                 <h2 className='card-title'>Check out modelcomp on PyPI!</h2>
                 <img className='minor-card-image' src='https://img.shields.io/pypi/v/modelcomp'/>
                 <img className='minor-card-image' src='https://img.shields.io/github/license/gilramot/modelcomp'/>
